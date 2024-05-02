b70b44d25968d86f44970f4be2ce2e02c0f4afba
Mina Sadek <mina730@gmail.com>
2024-05-02 22:13:22 +0200

    Add unit test

---
+++ src/Application/Carts/Commands/AddItemToTheCartCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CartingService.Application.Common.Interfaces;
+using CartingService.Domain.Entities;
+
+namespace CartingService.Application.CartItems.Commands;
+public record AddCartCommand : IRequest<string?>
+{
+    public string? CartId { get; init; }
+}
+
+class AddCartCommandHandler : IRequestHandler<AddCartCommand, string?>
+{
+    private readonly IApplicationDbContext _context;
+
+    public AddCartCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<string?> Handle(AddCartCommand request, CancellationToken cancellationToken)
+    {
+        var entity = new Cart
+        {
+            CartId=request.CartId
+        };
+
+        _context.Carts.Add(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return entity.CartId;
+    }
+}
---
+++ tests/Application.FunctionalTests/TodoItems/Commands/AddCartItemTests.cs
@@ -0,0 +1,57 @@
+﻿using CartingService.Application.CartItems.Commands;
+using CartingService.Application.Carts.Queries.GetCartItemsList;
+using CartingService.Application.Common.Exceptions;
+using CartingService.Domain.Entities;
+
+namespace CartingService.Application.FunctionalTests.TodoItems.Commands;
+
+using static Testing;
+
+public class AddCartItemTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldRequireMinimumFields()
+    {
+        var command = new AddItemToTheCartCommand();
+
+        await FluentActions.Invoking(() =>
+            SendAsync(command)).Should().ThrowAsync<ValidationException>();
+    }
+
+    [Test]
+    public async Task ShouldAddCartItem()
+    {
+        var userId = await RunAsDefaultUserAsync();
+
+        var listId = await SendAsync(new AddCartCommand
+        {
+            CartId = "Cart Id"
+        });
+
+        var command = new AddItemToTheCartCommand
+        {
+            CartId = listId,
+            ImageText = "Image Text",
+            ImageURL="Image URL",
+            Name = "Name",
+            Price=10,
+            Quantity=2
+        };
+
+        var itemId = await SendAsync(command);
+
+        var item = await FindAsync<Item>(itemId);
+
+        item.Should().NotBeNull();
+        item!.CartId.Should().Be(command.CartId);
+        item.Name.Should().Be(command.Name);
+        item.ImageText.Should().Be(command.ImageText);
+        item.ImageURL.Should().Be(command.ImageURL);
+        item.Price.Should().Be(command.Price);
+        item.Quantity.Should().Be(command.Quantity);
+        item.CreatedBy.Should().Be(userId);
+        item.Created.Should().BeCloseTo(DateTime.Now, TimeSpan.FromMilliseconds(10000));
+        item.LastModifiedBy.Should().Be(userId);
+        item.LastModified.Should().BeCloseTo(DateTime.Now, TimeSpan.FromMilliseconds(10000));
+    }
+}
