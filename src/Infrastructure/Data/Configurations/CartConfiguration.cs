cdf97b123bcb9e58af97151e76bd16c691f83df3
Mina Sadek <mina730@gmail.com>
2024-05-08 17:19:53 +0200

    Add database configuration

---
+++ src/Infrastructure/Data/Configurations/CartConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Emit;
+using CartingService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CatalogService.Infrastructure.Data.Configurations;
+
+public class CartConfiguration : IEntityTypeConfiguration<Cart>
+{
+    public void Configure(EntityTypeBuilder<Cart> builder)
+    {
+        builder.HasKey(a => a.CartId);
+        builder.Property(t => t.CartId)
+            .HasMaxLength(50)
+        .IsRequired();
+
+    }
+}
---
+++ src/Infrastructure/Data/Configurations/CartItemConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CartingService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CatalogService.Infrastructure.Data.Configurations;
+
+public class CartItemConfiguration : IEntityTypeConfiguration<Item>
+{
+    public void Configure(EntityTypeBuilder<Item> builder)
+    {
+        builder.Property(t => t.Name)
+            .HasMaxLength(50)
+            .IsRequired();
+        builder
+            .HasKey(b => b.Id);
+
+        builder
+            .Property(b => b.Price)
+            .IsRequired();
+        builder
+            .Property(b => b.Quantity).IsRequired();
+    }
+}
