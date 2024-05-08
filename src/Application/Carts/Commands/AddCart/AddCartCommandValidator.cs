444e41145a434c56d05febaff36093db0038ea87
Mina Sadek <mina730@gmail.com>
2024-05-08 17:28:36 +0200

    Add Cart Validation

---
+++ src/Application/Carts/Commands/AddCart/AddCartCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using CartingService.Application.Carts.Commands.AddCart;
+
+namespace CartingService.Application.CartItems.Commands.AddCartItemCommand;
+
+public class AddCartCommandValidator : AbstractValidator<AddCartCommand>
+{
+    public AddCartCommandValidator()
+    {
+        RuleFor(v => v.CartId)
+            .MaximumLength(50)
+            .NotEmpty();
+    }
+}
